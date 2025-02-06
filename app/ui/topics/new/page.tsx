f41e13adfcf4da7e68ed4347c31719c5184fa0b3
Frandy Slueue <frandy4ever@gmail.com>
2025-02-05 21:45:10 -0600

    Setup Newtopic page route

---
+++ app/ui/topics/new/page.tsx
@@ -0,0 +1,9 @@
+export default function NewTopic() {
+    return (
+      <main>
+        <h1>Create a New Topic</h1>
+        <p>A form will go here for adding a new topic.</p>
+      </main>
+    );
+  }
+  
