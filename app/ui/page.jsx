8aa3b76dc7cb75e3d5ae2954d5f3c9c503b56bc1
Frandy Slueue <frandy4ever@gmail.com>
2025-02-05 21:27:29 -0600

    Setup UserHome page route

---
+++ app/ui/page.jsx
@@ -0,0 +1,9 @@
+export default function UserHome() {
+    return (
+      <main>
+        <h1>User Dashboard</h1>
+        <p>Welcome back! Explore topics and ask questions.</p>
+      </main>
+    );
+  }
+  
