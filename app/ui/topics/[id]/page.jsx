ca4c70321f100a8ce97daec3c37ec96c34406a8b
Frandy Slueue <frandy4ever@gmail.com>
2025-02-05 21:40:34 -0600

    Setup Topic details page route

---
+++ app/ui/topics/[id]/page.jsx
@@ -0,0 +1,13 @@
+interface TopicPageProps {
+    params: { id: string };
+  }
+  
+  export default function TopicPage({ params }: TopicPageProps) {
+    return (
+      <main>
+        <h1>Topic: {params.id}</h1>
+        <p>List of questions related to this topic will be displayed here.</p>
+      </main>
+    );
+  }
+  
